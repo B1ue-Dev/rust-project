6351efcbbd29c2a5e49205c096601e320a04029e
Blue <60958064+B1ue-Dev@users.noreply.github.com>
2023-07-12 17:48:11 +0700

    feat: Guessing Game

---
+++ guessing_game/main.rs
@@ -0,0 +1,38 @@
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(0..=10);
+
+    println!("Guess the number game");
+
+    loop {
+        println!("Please input your guess: ");
+
+        let mut guess = String::new();
+
+        std::io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a number, please try again.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small. Try again."),
+            Ordering::Equal => {
+                println!("You got it right.");
+                break;
+            }
+            Ordering::Greater => println!("Too big. Try again."),
+        }
+    }
+}
